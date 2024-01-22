5ff6e9b0788a01175e7e2a24c388b8dc69b662d1
Valentin Olberding <va.olberding@gmail.com>
2024-01-22 14:56:58 +0100

    add-task fix

--- js/add_task.js
+++ js/add_task.js
@@ -11,3 +11,53 @@ function resetAddTaskBoard() {
         
     }
 }
+
+
+function activeButtonUrgent(){
+    document.getElementById('urgent').classList.add('active-urgent')
+    document.getElementById('medium').classList.remove('active-medium');
+    document.getElementById('low').classList.remove('active-low');
+    document.getElementById('urgent-white').classList.remove('d-none');
+    document.getElementById('urgent-black').classList.add('d-none');
+    document.getElementById('medium-white').classList.add('d-none');
+    document.getElementById('medium-black').classList.remove('d-none');
+    document.getElementById('low-white').classList.add('d-none');
+    document.getElementById('low-black').classList.remove('d-none');
+    
+}
+
+function activeButtonMedium(){
+    document.getElementById('medium').classList.add('active-medium');
+    document.getElementById('urgent').classList.remove('active-urgent');
+    document.getElementById('low').classList.remove('active-low');
+    document.getElementById('urgent-white').classList.add('d-none');
+    document.getElementById('urgent-black').classList.remove('d-none');
+    document.getElementById('medium-white').classList.remove('d-none');
+    document.getElementById('medium-black').classList.add('d-none');
+    document.getElementById('low-white').classList.add('d-none');
+    document.getElementById('low-black').classList.remove('d-none');
+}
+
+function activeButtonLow(){
+    document.getElementById('medium').classList.remove('active-medium');
+    document.getElementById('urgent').classList.remove('active-urgent');
+    document.getElementById('low').classList.add('active-low');
+    document.getElementById('urgent-white').classList.add('d-none');
+    document.getElementById('urgent-black').classList.remove('d-none');
+    document.getElementById('medium-white').classList.add('d-none');
+    document.getElementById('medium-black').classList.remove('d-none');
+    document.getElementById('low-white').classList.remove('d-none');
+    document.getElementById('low-black').classList.add('d-none');
+}
+
+function resetActiveButtonBySubmit(){
+    document.getElementById('medium').classList.remove('active-medium');
+    document.getElementById('urgent').classList.remove('active-urgent');
+    document.getElementById('low').classList.remove('active-low');
+    document.getElementById('urgent-white').classList.add('d-none');
+    document.getElementById('urgent-black').classList.remove('d-none');
+    document.getElementById('medium-white').classList.add('d-none');
+    document.getElementById('medium-black').classList.remove('d-none');
+    document.getElementById('low-white').classList.add('d-none');
+    document.getElementById('low-black').classList.remove('d-none');
+}
